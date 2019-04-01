74e4be87612dc6be304e9ae0b9a33f0cc51db63d
RMCProjectFiles <rmchappell@gmail.com>
2019-04-01 15:13:38 +0100

    Homework: Lecture 1 - Data Types

--- song.js
+++ song.js
@@ -7,12 +7,12 @@
 
 //create an object to hold details of the song
 var song = {
-    songName: "Happy", //String value
-    artist: "Pharrell Williams", //String value
-    album: "Happy", //String value
-    duration: 233, //Numeric value representing the duration of the song in seconds
-    isEarWorm: true, //Boolean value
-    melancholy: false, //Boolean value
+    songName: "Happy", // String value
+    artist: "Pharrell Williams", // String value
+    album: "Happy", // String value
+    duration: 233, // Numeric value representing the duration of the song in seconds
+    isEarWorm: true, // Boolean value
+    melancholy: false, // Boolean value
     chartPositions: [
         {
             chart: "UK Singles", // Key = chart, value = "UK Singles"
@@ -30,15 +30,15 @@ var song = {
             chart: "Hungary Dance Top 40", // Key = chart, value = "Hungary Dance Top 40"
             position: 4, // Key = position, value = 4
         }
-    ], //Array of objects
+    ], // Array of objects
     credits: {
-        writtenBy: "Pharrell Williams", //String value
-        producedBy: "Pharrell Williams", //String value
+        writtenBy: "Pharrell Williams", // String value
+        producedBy: "Pharrell Williams", // String value
     }, // Object to hold details of the song credits
-    copiesSold: 1922000, //Numeric value,
-    released: "November 21, 2013", //String value
-    recordingYear: 2013, //Numeric value
-    formats: ["7 inch", "12 inch", "CD", "Digital Download"], //Array of formats that the song was released in - all string values
+    copiesSold: 1922000, // Numeric value,
+    released: "November 21, 2013", // String value
+    recordingYear: 2013, // Numeric value
+    formats: ["7 inch", "12 inch", "CD", "Digital Download"], // Array of formats that the song was released in - all string values
 }
 
 /* Access and output values of the song object*/
@@ -48,7 +48,7 @@ console.log(song.artist); // 'Pharrell Williams' - string
 console.log(song.album); // 'Happy' - string
 console.log(song.duration); // 233 - number
 console.log(song.isEarWorm); // true - boolean
-console.log(song.melancholy); // false boolean
+console.log(song.melancholy); // false - boolean
 console.log(song.chartPositions) // Array of objects
 console.log(song.chartPositions[0].chart); // Accessing the first object in the array and outputting the value assigned to the "chart" key - "UK Singles" - string
 console.log(song.chartPositions[0].position); // Accessing the first object in the array and outputting the value assigned to the "position" key - 1 - number
